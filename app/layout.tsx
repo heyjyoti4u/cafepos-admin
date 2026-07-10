7985e19e0bb51363febd76b582f310a0bec18cd7
Jyotiranjan Mohanty <jyotimohanty289@gmail.com>
2026-07-10 10:03:14 +0530

    Add OneSignalInit component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <OneSignalInit />
         {children}
         <CookieConsent />
       </body>
